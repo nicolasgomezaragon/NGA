b1a9a4011faf86265a8754f82f92959c65e1ab89
Nick <nicolasgomezaragon@proton.me>
2025-03-25 15:54:55 -0500

    Basic server features created, now checking express to further integrate backend and frontend

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req,res) => {
+    res.send('Hello world!')
+});
+
+app.get('/about', (req, res) => {
+    res.send('About me! NGA!')
+})
+
+app.get('/portfolio', (req,res) => {
+    res.send('On going projects!')
+})
+
+app.get('/contact', (req,res) =>{
+    res.send('How to reach me!')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running at https://localhost:{pot}`);
+});
+
